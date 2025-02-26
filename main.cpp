152ce3d1821a3877cc2527edff0d7b3d53743784
Petros Arvanitis <petros.arvanitis@mvg-world.com>
2025-02-26 13:00:59 +0200

    feat(Node): Create class Node for Trie data structure.
    -Include key, validWord and children fields
    - Implement constructor using parent node and letter
    - Implement getKey() and setValidWord() function

--- main.cpp
+++ main.cpp
@@ -10,14 +10,61 @@ struct Board
 
     char get_letter(size_t row, size_t col)
     {
+        // Don't use vector.at() for efficiency
         return table[row*columns + col]; // row-major
     }
 };
 
+// A node contains
+//  1) key: a sequence of characters
+//  2) validWord: a bool indicating whether it's a valid word
+//  3) children: a vector of pointers to children nodes
+class Node
+{
+    private:
+        std::string key{};
+        bool validWord{false};
+        std::vector<Node*> children{};
 
+    public:
+        // Constructor from another Node + char
+        Node(Node parent, char letter) 
+        {
+            this->key = parent.getKey() + letter;
+        } 
+
+        std::string getKey() const 
+        {
+            return this->key;
+        }
+
+        void setValidWord() 
+        {
+            this->validWord = true;
+        }
+};
+
+// Search tree data structure used to store and retrieve strings from a dictionary or set
+// Unlike a binary search tree, nodes in a trie do not store their associated key. Instead, 
+// each node's position within the trie determines its associated key, with the connections
+// between nodes defined by individual characters rather than the entire key.
+// source: https://en.wikipedia.org/wiki/Trie#:~:text=In%20computer%20science%2C%20a%20trie,from%20a%20dictionary%20or%20set.
 struct trie
 {
-
+    Node* root{};
+    // The trie would be constructed as following using a number of words:
+    // 1. Initialize empty root node
+    // 2. Iterate over each word:
+    //  i)      Initiate current node to root node
+    //  ii)     Initiate current word to ''
+    //  iii)    Iterate over letters:
+    //              - Add letter to current word
+    //              - Check if the key of the active word belongs to any of the current node's children
+    //                  - Yes: traverse to the child node with the key in question
+    //                  - No: create the child node with the key in question and traverse to it
+    // iv)      Is the word complete?
+    //              - Yes: set node as valid word
+    //              - No: nothing (continue to next letter)
 };
 
 
